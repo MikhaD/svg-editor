5baf890f26a47270481ebe691de03ffe0616b216
MikhaD <mikhadavids@gmail.com>
2023-02-17 03:40:02 +0200

    fix: :rocket: added base in config to prevent deployment issues

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { svelte, vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: "/svg-editor/",
 	plugins: [svelte({
 		// hot: !process.env.VITEST,
 		preprocess: vitePreprocess(),
